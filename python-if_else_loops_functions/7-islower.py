4f8405973ab13eabd8905d8b1d72cc805ae5a0c8
Julio <julio.1515.pr@gmail.com>
2024-05-08 20:41:55 -0400

    adding task0 to task8 already tested

---
+++ python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def islower(c):
+    if "a" <= c <= "z":
+        return True
+    else:
+        return False
---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uppercase(s):
+    result = ""
+    for char in s:
+        if 'a' <= char <= 'z':
+            result += chr(ord(char) - 32)
+        else:
+            result += char
+    print("{}".format(result))
