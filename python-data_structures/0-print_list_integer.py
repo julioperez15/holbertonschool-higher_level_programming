b3146eb0587d6d9b6202554e9999e537f4600345
Julio <julio.1515.pr@gmail.com>
2024-05-13 19:26:32 -0400

    fix task0 and task1

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-#0-print_list_integer.py
-## Julio Perez <9034@holbertonschool.com>
 
 def print_list_integer(my_list=[]):
     for i in my_list:
--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-#1-element_at.py
-## Julio Perez <9034@holbertonschool.com>
+
 
 def element_at(my_list, idx):
     if 0 <= idx < len(my_list):
