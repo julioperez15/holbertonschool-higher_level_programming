4cf2bdb7a877e9df97f80c3ebea7dec1f56e1c3b
Julio <julio.1515.pr@gmail.com>
2024-05-13 18:34:15 -0400

    task0

---
+++ python-data_structures/0-print_list_integer.py
@@ -0,0 +1,7 @@
+#usr/nin/env python3
+#0-print_list_integer.py
+## Julio Perez <9034@holbertonschool.com>
+
+def print_list_integer(my_list=[]):
+    for i in my_list: 
+        print("{:d}".format(i))
