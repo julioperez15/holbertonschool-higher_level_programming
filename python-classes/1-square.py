89cc4185629c365abfcd1f6c50961d177e42f8cf
Julio <julio.1515.pr@gmail.com>
2024-05-17 21:47:07 -0400

    adding documentation task 1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python3
+"""
+    This module defines a class Square.
+
+    This module demonstrates the creation of a cass in Python that initializes
+    a private instance attribute: size.
+    """
+
 
 class Square:
-    def __init__(self, size=0):
+    """
+    This is a class that defines a square.
+
+    Attributes:
+         __size (int): size of the side of the square.
+    """
+    def __init__(self, size):
+        """
+        The constructor for Square class.
+
+        Parameters:
+            size (int): size of the side of the square.
+
+        """
         self.__size = size
-
-    @property
-    def size(self):
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        if type(value) is not int:
-            raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = value
-
-    def area(self):
-        return self.__size ** 2
