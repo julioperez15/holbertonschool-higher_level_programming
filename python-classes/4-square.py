3b88f7a518bf4ba9310898e67d9d3d9997d041ef
Julio <julio.1515.pr@gmail.com>
2024-05-17 21:51:18 -0400

    adding documentation task 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,27 +1,32 @@
 #!/usr/bin/python3
+"""
+Module for Square class.
+"""
+
 
 class Square:
+    """
+    Square class with size attribute and area calculation.
+    """
+
     def __init__(self, size=0):
+        """Initialize Square with optional size."""
         self.__size = size
 
     def area(self):
+        """Calculate and return the area of the Square."""
         return self.__size ** 2
 
     @property
     def size(self):
+        """Get the size of the Square."""
         return self.__size
 
     @size.setter
     def size(self, value):
-        if type(value) is not int:
+        """Set the size of the Square, with checks for valid size"""
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError("size must be >=0")
         self.__size = value
-
-    def my_print(self):
-        if self.__size == 0:
-            print()
-        else:
-            for i in range(self.__size):
-                print("#" * self.__size)
