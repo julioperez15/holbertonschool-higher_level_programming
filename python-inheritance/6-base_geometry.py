99490cc45d608fba4ec5083effff8cc1f03e6328
Julio <julio.1515.pr@gmail.com>
2024-05-22 16:31:22 -0400

    adding task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a class BaseGeometry."""
+
+
+class BaseGeometry:
+    def area(self):
+        """Not implemented"""
+        raise Exception("area() is not implemented")
