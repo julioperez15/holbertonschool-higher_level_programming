f6503d7ddb4e7e79c6b416d7e3cb75a608ad6cd7
Julio <julio.1515.pr@gmail.com>
2024-05-22 15:54:59 -0400

    adding task 1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+"""Defines an inherited list class MyList."""
 
 
 class MyList(list):
-    """A class that inherits from the list class"""
+    """Implements sorted printing for the built-in list class."""
+
     def print_sorted(self):
-        """Prints the list, but sorted"""
+        """Print a list in sorted ascending order."""
         print(sorted(self))
