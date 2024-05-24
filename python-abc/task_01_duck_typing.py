a6238ae413350003483529f941e4b5c3cb4556ea
Julio <julio.1515.pr@gmail.com>
2024-05-24 16:02:56 -0400

    adding task 01

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -15,6 +15,8 @@ class Shape(ABC):
 
 class Circle(Shape):
     def __init__(self, radius):
+        if radius < 0:
+            raise ValueError("Radius cannot be negative")
         self.radius = radius
 
     def area(self):
