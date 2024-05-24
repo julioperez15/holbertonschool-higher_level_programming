07f85eb24447f79fe794e7d0b957c868a069938d
Julio <julio.1515.pr@gmail.com>
2024-05-24 15:30:18 -0400

    adding task 00

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+
+rom abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    @abstractmethod
+    def sound(self):
+        pass
+
+
+class Dog(Animal):
+    """
+    A class representing a dog.
+
+    Attributes:
+        _sound (str): The sound that a dog makes.
+    """
+
+    def __init__(self):
+        self._sound = "Bark"
+
+    def sound(self):
+        """
+        Returns the sound that a dog makes.
+
+        Returns:
+            str: The sound of a dog.
+        """
+        return self._sound
+
+
+class Cat(Animal):
+    def __init__(self):
+        self._sound = "Meow"
+
+    def sound(self):
+        return self._sound
