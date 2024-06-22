eeb3520030064ad5f7d60986b63296a1e0dd65f5
Julio <julio.1515.pr@gmail.com>
2024-06-21 21:01:47 -0400

    task 2 fix

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,26 +1,31 @@
 #!/usr/bin/python3
-"""script that takes an argument and displays all values in the states table"""
-
-
+"""script that takes in an argument and displays all values in the
+states table of hbtn_0e_0_usa
+where name matches the argument."""
 import MySQLdb
-import sys
+from sys import argv
 
-if __name__ == "__main__":
-    """ establishes a connection to the database """
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database_name = sys.argv[3]
-    state_name_searched = sys.argv[4]
+if __name__ == '__main__':
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+    state_name = argv[4]
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=username,
-                         passwd=password, db=database_name)
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name
+        )
 
     cursor = db.cursor()
-    query = "SELECT * FROM states WHERE name = %s ORDER BYE id ASC"
-    cursor.execute(query, (sys.argv[4],))
+    query = "SELECT * FROM states WHERE\
+        BINARY name LIKE '{}'".format(state_name)
+    cursor.execute(query)
     rows = cursor.fetchall()
     for row in rows:
         print(row)
-    # Close the cursor and database connection outside of the loop
+
     cursor.close()
     db.close()
