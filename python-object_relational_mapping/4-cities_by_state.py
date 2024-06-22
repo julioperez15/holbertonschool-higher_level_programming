54b4c547a6cbfa6a8037abc018a545b84945a96d
Julio <julio.1515.pr@gmail.com>
2024-06-21 21:04:28 -0400

    task 4 fix

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,35 +1,33 @@
 #!/usr/bin/python3
-"""This script lists all cities from a given MySQL database."""
 
+"""Write a script that lists all cities from the database hbtn_0e_4_usa"""
 
 import MySQLdb
 from sys import argv
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     username = argv[1]
     password = argv[2]
-    database_name = argv[3]
+    db_name = argv[3]
 
-    # Connect to the MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306, user=username,
-                         passwd=password, db=database_name)
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name
+    )
 
-    # Creates a cursor object
-    cur = db.cursor()
+    cursor = db.cursor()
 
-    # Execute the SQL query
-    cur.execute("""SELECT cities.id, cities.name, states.name
-                FROM cities
-                JOIN states ON cities.state_id = states.id
-                ORDER BY cities.id ASC""")
+    query = "SELECT cities.id, cities.name, states.name FROM\
+        cities JOIN states ON cities.state_id = states.id"
+    cursor.execute(query)
 
-    # Fetch all rows from the result
-    rows = cur.fetchall()
+    rows = cursor.fetchall()
 
-    # Print each row
     for row in rows:
         print(row)
 
-    # Close the cursor and the database connection
-    cur.close()
+    cursor.close()
     db.close()
