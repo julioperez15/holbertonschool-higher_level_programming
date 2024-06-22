5ddc408214366ed02bcd63fc83596ceb6c2f1121
Julio <julio.1515.pr@gmail.com>
2024-06-21 20:56:40 -0400

    task 2 fix

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-"""script that takes an argument and displays all values in the states table"""
-
+""" Write a script that list all states with the same name starting with N """
 
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
     """ establishes a connection to the database """
-    db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
-
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
-    id ASC".format(argv[4]))
-    rows = cursor.fetchall()
-    for row in rows:
-        if row[1] == argv[4]:
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    records = cursor.fetchall()
+    for row in records:
+        if row[1][0] == 'N':
             print(row)
-        cursor.close()
-        db.close()
+    cursor.close()
+    db.close()
