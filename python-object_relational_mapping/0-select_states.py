73e7322c128316789eb8f770a424cd0a38756e0a
Julio <julio.1515.pr@gmail.com>
2024-06-21 20:47:58 -0400

    task 0 fix

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,26 +1,9 @@
-#!/usr/bin/python3
-"""script that lists all states from the database"""
-
-
-import MySQLdb
-from sys import argv
-
-if __name__ == "__main__":
-    username = argv[1]
-    password = argv[2]
-    database_name = argv[3]
-
-    db = MySQLdb.connect(host="localhost", port=3306, user=username,
-                         passwd=password, db=database_name)
-
-    cur = db.cursor()
-
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-
-    rows = cur.fetchall()
-
-    for row in rows:
-        print(row)
-
-    cur.close()
-    db.close()
+-- Create states table in hbtn_0e_0_usa with some data
+CREATE DATABASE IF NOT EXISTS hbtn_0e_0_usa;
+USE hbtn_0e_0_usa;
+CREATE TABLE IF NOT EXISTS states ( 
+    id INT NOT NULL AUTO_INCREMENT, 
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+);
+INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
